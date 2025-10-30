4a2e88186ffd7eaddbddd9c9c750d37e9c9bf389
Abdal Hussain <abdalhussain5790@gmail.com>
2025-10-30 22:27:56 +0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,7 +75,15 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
         quantity: 1
       });
     }
-    
+
+    // Calculate total cart Items quantity
+    let cartQuantity = 0;
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+    })
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
     console.log(cart);
     
   })
