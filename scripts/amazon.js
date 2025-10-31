fc773ec7942590e7810647f700f6a2053ea87ce9
Abdal Hussain <abdalhussain5790@gmail.com>
2025-10-31 19:10:42 +0500

    Add Select Quantity Feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -60,6 +60,7 @@ document.querySelector(".js-products-grid").innerHTML = productHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId = button.dataset.productId; // get name from data-attribute
+    const quantitySelect = Number(document.querySelector(`.js-quantity-selector-${productId}`).value); // Quantity based on selection
 
     let matchingItem;
     cart.forEach((item)=>{
@@ -68,11 +69,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
       }
     })
     if(matchingItem){
-      matchingItem.quantity += 1; // increment quantity by 1 if item matches
+      matchingItem.quantity =  quantitySelect;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantitySelect
       });
     }
 
