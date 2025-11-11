86e14273c5c27e6a1ac315c226eacdb229f8d03c
Abdal Hussain <abdalhussain5790@gmail.com>
2025-11-11 18:47:55 +0500

    Seperate HTML

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -1,12 +1,26 @@
 import { calculateCartQuantity } from "../../data/cart.js";
 
 export function renderCheckoutHeader(){
+    const cartQuantity = calculateCartQuantity();
 
-    function updateCartQuantity(){
-        const cartQuantity = calculateCartQuantity();
+    const checkoutHeaderHTML = `
+        <div class="header-content">
+        <div class="checkout-header-left-section">
+          <a href="amazon.html">
+            <img class="amazon-logo" src="images/amazon-logo.png">
+            <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+          </a>
+        </div>
 
-        document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
-    }
+        <div class="checkout-header-middle-section">
+          Checkout (<a class="return-to-home-link js-return-to-home-link"
+            href="amazon.html">${cartQuantity}</a>)
+        </div>
 
-    updateCartQuantity();
+        <div class="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png">
+        </div>
+      </div>
+    `
+    document.querySelector('.js-checkout-header').innerHTML = checkoutHeaderHTML;
 }
